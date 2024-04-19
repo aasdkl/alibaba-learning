218e1887671e59be5389a29e1c5b8f41c6dc656d
hsueh chiahao <hsuehchiahao@gmail.com>
2024-04-19 17:36:58 +0800

    add necos config on product service /get

--- service-product/src/main/java/stock/ProductApplication.java
+++ service-product/src/main/java/stock/ProductApplication.java
@@ -1,12 +1,23 @@
 package stock;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class ProductApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProductApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext context = SpringApplication.run(ProductApplication.class, args);
+        ConfigurableEnvironment environment = context.getEnvironment();
+        while (true) {
+            String id = environment.getProperty("demo.id");
+            String name = environment.getProperty("demo.name");
+            System.out.println("id=" + id + "; name=" + name);
+            TimeUnit.SECONDS.sleep(3);
+        }
     }
 }
--- service-product/src/main/java/stock/controller/ProductController.java
+++ service-product/src/main/java/stock/controller/ProductController.java
@@ -1,14 +1,14 @@
 package stock.controller;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/product")
+@RefreshScope
 public class ProductController {
 
 //    @Autowired
@@ -18,9 +18,22 @@ public class ProductController {
     @Value("${server.port}")
     String port;
 
+    @Value("${demo.id}")
+    String demoId;
+
+    @Value("${demo.name}")
+    String demoName;
+
+    // feign 不支持多路径，所以两个 get 方法不能合并
     @RequestMapping("/get/{id}")
     public String get(@PathVariable int id) {
         System.out.println("商品 " + id);
         return "[product " + id + " by "+ port + "]";
     }
+
+    @RequestMapping("/get")
+    public String get() {
+        return "[product " + demoId + "-" + demoName + "]";
+    }
+
 }
